2d5a386fe03626cf56a7158ca4de3da6b2486c63
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-05-22 22:32:34 -0700

    Adding App Bar Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/presentation/color_manager.dart';
+import 'package:tut_app/presentation/font_manager.dart';
+import 'package:tut_app/presentation/styles_manager.dart';
 import 'package:tut_app/presentation/valus_manager.dart';
 
 ThemeData getAppTheme() {
@@ -9,11 +11,23 @@ ThemeData getAppTheme() {
     primaryColorLight: ColorManager.lightPrimary,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.lightPrimary, // ripple effect color
     // cardview theme
     cardTheme: CardTheme(
       color: ColorManager.white,
       shadowColor: ColorManager.grey,
       elevation: AppSize.s4,
+    ),
+    // appbar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle: getRegularStyle(
+        color: ColorManager.white,
+        fontSize: FontSize.s16,
+      ),
     ),
   );
 }

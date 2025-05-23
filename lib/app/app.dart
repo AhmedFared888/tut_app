a3f0cf65b516c8ec964df929c9f06c6af067cbb2
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-05-22 22:00:05 -0700

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  //named construtor
+  MyApp._internal();
+  static final MyApp _instance =
+      MyApp._internal(); // singleton or single instance
+  factory MyApp() => _instance; // factory constructor
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

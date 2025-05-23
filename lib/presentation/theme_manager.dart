14e388904f978bdf6b87678bfd8eb5666c03c443
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-05-22 22:17:49 -0700

    create theme manager & main colors

---
+++ lib/presentation/theme_manager.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/color_manager.dart';
+
+ThemeData getAppTheme() {
+  return ThemeData(
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+  );
+}

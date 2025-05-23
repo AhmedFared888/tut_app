2cfd485d4f4f2848a0a7572b4fc1fd52e66bee02
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-05-23 00:45:20 -0700

    Finish Adding Routes Manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/forget_password/forget_password_view.dart';
+import 'package:tut_app/presentation/login/login_view.dart';
+import 'package:tut_app/presentation/main/main_view.dart';
+import 'package:tut_app/presentation/register/register_view.dart';
+import 'package:tut_app/presentation/splash/splash_view.dart';
+import 'package:tut_app/presentation/store_details/store_details_view.dart';
+
 class RoutesManager {
   static const String splashRoute = '/';
   static const String loginRoute = '/login';
@@ -5,4 +13,35 @@ class RoutesManager {
   static const String forgetPasswordhRoute = '/forgetPassword';
   static const String mainRoute = '/main';
   static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesManager.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case RoutesManager.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case RoutesManager.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case RoutesManager.forgetPasswordhRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordView());
+      case RoutesManager.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case RoutesManager.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text('No Route Found'),
+              ),
+              body: const Center(child: Text('No Route Found')),
+            ));
+  }
 }

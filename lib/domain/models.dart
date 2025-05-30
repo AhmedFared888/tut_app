4c0a136ca46164f34b9253f914eaccfedabe95ca
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-05-30 15:31:23 -0700

    Creating Models

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -14,3 +14,25 @@ class SliderViewObject {
   final int currentIndex;
   SliderViewObject(this.sliderObject, this.numOfSlides, this.currentIndex);
 }
+
+// Login models
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+  Authentication(this.customer, this.contacts);
+}

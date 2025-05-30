4b83a9f9a7f1d2dbce496ab1e0dfc1b41647866c
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-05-30 15:30:47 -0700

    Creating Models

--- lib/data/network/app_api.g.dart
+++ lib/data/network/app_api.g.dart
@@ -9,7 +9,9 @@ part of 'app_api.dart';
 // ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _AppServiceClient implements AppServiceClient {
-  _AppServiceClient(this._dio, {this.baseUrl, this.errorLogger});
+  _AppServiceClient(this._dio, {this.baseUrl, this.errorLogger}) {
+    baseUrl ??= 'https://ge3zr.wiremockapi.cloud/';
+  }
 
   final Dio _dio;
 

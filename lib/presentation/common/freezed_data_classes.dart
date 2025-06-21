a9faac2006772cc7ef3deb4532f4ac08a6143d57
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-21 14:40:07 -0700

    Binding Login ViewModel to Login View

--- lib/presentation/common/freezed_data_classes.dart
+++ lib/presentation/common/freezed_data_classes.dart
@@ -6,4 +6,49 @@ part 'freezed_data_classes.freezed.dart';
 @freezed
 class LoginObject with _$LoginObject {
   factory LoginObject(String userName, String password) = _LoginObject;
+
+  @override
+  List<DiagnosticsNode> debugDescribeChildren() {
+    // TODO: implement debugDescribeChildren
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement password
+  String get password => throw UnimplementedError();
+
+  @override
+  DiagnosticsNode toDiagnosticsNode(
+      {String? name, DiagnosticsTreeStyle? style}) {
+    // TODO: implement toDiagnosticsNode
+    throw UnimplementedError();
+  }
+
+  @override
+  String toStringDeep(
+      {String prefixLineOne = '',
+      String? prefixOtherLines,
+      DiagnosticLevel minLevel = DiagnosticLevel.debug,
+      int wrapWidth = 65}) {
+    // TODO: implement toStringDeep
+    throw UnimplementedError();
+  }
+
+  @override
+  String toStringShallow(
+      {String joiner = ', ',
+      DiagnosticLevel minLevel = DiagnosticLevel.debug}) {
+    // TODO: implement toStringShallow
+    throw UnimplementedError();
+  }
+
+  @override
+  String toStringShort() {
+    // TODO: implement toStringShort
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement userName
+  String get userName => throw UnimplementedError();
 }

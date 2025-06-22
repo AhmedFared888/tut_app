6d815b581e0fc26fc5da90ba27f5d8b8c7210d21
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-22 00:32:30 -0700

    Add dependency injection Instances

--- lib/app/dependency_injection.dart
+++ lib/app/dependency_injection.dart
@@ -1,6 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tut_app/app/app_prefs.dart';
+import 'package:tut_app/data/data_source/remote_data_source.dart';
+import 'package:tut_app/data/network/app_api.dart';
+import 'package:tut_app/data/network/dio_factory.dart';
+import 'package:tut_app/data/network/network_info.dart';
+import 'package:tut_app/data/repository/repository_impl.dart';
+import 'package:tut_app/domain/repos/repo.dart';
 
 final instance = GetIt.instance;
 
@@ -14,4 +22,25 @@ Future<void> initAppModule() async {
 
   // app prefs instance
   instance.registerLazySingleton<AppPrefs>(() => AppPrefs(instance()));
+
+  //network info
+  instance.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(
+      InternetConnectionChecker
+          .createInstance())); // عشان النيتورك انفو ابستراكت كلاس استخدمنا الامبلمينتر بتاعه
+
+  // dio factory
+  instance.registerLazySingleton<DioFactory>(() => DioFactory(instance()));
+
+  Dio dio = await instance<DioFactory>().getDio();
+
+  // app service client
+  instance.registerLazySingleton<AppServiceClient>(() => AppServiceClient(dio));
+
+  //remote data source
+  instance.registerLazySingleton<RemoteDataSource>(
+      () => RemoteDataSourceImpl(instance()));
+
+  // repo
+  instance.registerLazySingleton<Repo>(
+      () => RepositoryImpl(instance(), instance()));
 }
